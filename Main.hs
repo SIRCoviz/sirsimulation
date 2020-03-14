a8f8a0e6a469dbe3b155775dcac8b8b2e8de2a2e
Shae Erisson <shae@scannedinavian.com>
2020-03-14 15:54:48 -0400

    initial quick hack of servant back end, only produces static data

--- Main.hs
+++ Main.hs
@@ -1,8 +1,78 @@
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE DeriveAnyClass             #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE RankNTypes                 #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE TypeOperators              #-}
 module Main where
 
-import qualified MyLib (someFunc)
+import           Prelude                       ()
+import           Prelude.Compat
+
+import           Control.Monad.Except
+import           Control.Monad.Reader
+import           Data.Aeson
+import qualified Data.Aeson.Parser
+import           Data.Aeson.Types
+import           Data.Attoparsec.ByteString    hiding (take)
+import           Data.ByteString               (ByteString)
+import           Data.List
+import           Data.Maybe
+import           Data.String.Conversions
+import           Data.Time.Calendar
+import           GHC.Generics
+import           Lucid
+import           Network.HTTP.Media            ((//), (/:))
+import           Network.Wai
+import           Network.Wai.Handler.Warp
+import           Servant
+import           Servant.Types.SourceT         (source)
+import           System.Directory
+import           Text.Blaze
+import qualified Text.Blaze.Html
+import           Text.Blaze.Html.Renderer.Utf8
+
+data SIRD = Susceptible | Infected | Recovered | Dead deriving (Generic, ToJSON, Enum)
+
+data C19State = S [[SIRD]] deriving (Generic, ToJSON)
+
+data InitialState = IS
+    { initialCases :: Int
+    , scaling      :: Int
+    } deriving Generic
+
+instance ToJSON InitialState
+
+data Step = Step
+    { time  :: Int
+    , cells :: C19State
+    } deriving (Generic, ToJSON)
+
+type API = "initialstate" :> Capture "initialCases" Int :> Capture "scaling" Int :> Get '[JSON] [Step]
+
+server :: Server API
+server = calcState
+    where calcState :: Int -> Int -> Handler [Step]
+          calcState start scale = pure $ someSteps
+
+simAPI :: Proxy API
+simAPI = Proxy
+
+app :: Application
+app = serve simAPI server
 
 main :: IO ()
-main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+main = run 8080 app
+
+someSteps :: [Step]
+someSteps = boringStep <$> [1..50]
+    where boringStep t = Step { time = t, cells = someState }
+
+-- start out with 50 x 50 grid
+someState :: C19State
+someState = S $ replicate 50 fiftycols
+    where fiftycols = take 50 $ cycle [Susceptible .. Dead]
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # sirsimulation
 SIR model for simulation
+
+# What's left to do?
+
+- [ ] hook up URL captures to the model
+- [ ] write the model
+- [ ] compare to actual statistics
+
+# What URL to check?
+
+http://localhost:8080/initialstate/5/10
+
+# What does backend send to frontend?
+
+currently producing:
+
+``` json
+[ { time: 0, cells: [ [ "Susceptible", "Infected" ], [ "Dead, "Recovered" ] ] }
+```
+
+original suggestion
+``` json
+[ { time: 0, cells: [ { x: 0, y: 0, sir: "Susceptible" }, { x: 0, y: 1, sir: "Infected" }, ... ] }
+, { time: 1, cells: [ { x: 0, y: 0, sir: 0.1 }, { x: 0, y: 1, sir: 0.2 }, ... ] }
+, ... ]
+```
+
+# How does it work?
+
+This is mostly stolen from https://en.wikipedia.org/wiki/Epidemic_models_on_lattices#SIR_model
+
+The `z` column tells you how many neighbors
