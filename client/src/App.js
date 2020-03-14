db7b16b876465648836aa318627299c5d7e2fabd
Gabriel Duquette <gabriel@simspace.com>
2020-03-14 18:48:39 -0400

    initial ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,106 @@
 import React from 'react'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
+import {
+  createMuiTheme,
+  ThemeProvider,
+  makeStyles,
+} from '@material-ui/core/styles'
+
+import {
+  Container,
+  Header,
+  TopRow,
+  TopPane,
+  TopPaneHeader,
+  BottomRow,
+  TopPaneRow,
+  MapGraphContainer,
+  Map,
+  Graph,
+  SubHeader,
+  Subtext,
+} from './styles'
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: 'Merriweather',
+  },
+  overrides: {
+    MuiInput: {
+      text: {
+        // Some CSS
+        color: 'white',
+      },
+    },
+  },
+})
 
 export const App = () => {
-  return <h1>SIR Simulation</h1>
+  return (
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Header>COVID-19 SIRmulation</Header>
+        <SubHeader>
+          This tool allows you to simulate the progression of the COVID19 virus
+          using the SIR model.
+        </SubHeader>
+        <TopRow>
+          <TopPane>
+            <TopPaneHeader>Initial Data</TopPaneHeader>
+            <TopPaneRow>
+              <TextField
+                id="filled-basic"
+                variant="filled"
+                placeholder="Location"
+                size="small"
+                margin="dense"
+              />
+            </TopPaneRow>
+            <Subtext>population which you would like to simulate</Subtext>
+            <TopPaneRow>
+              <TextField
+                id="filled-basic"
+                variant="filled"
+                placeholder="Current Cases"
+              />
+            </TopPaneRow>
+            <Subtext>as of today, March 14th, 2019</Subtext>
+            <TopPaneHeader>Behavior Adjustments</TopPaneHeader>
+            <TopPaneRow>
+              <TextField
+                id="filled-basic"
+                variant="filled"
+                placeholder="Pre-COVID Interactions"
+                size="small"
+              />
+            </TopPaneRow>
+            <Subtext>social interactions per day, week, or month</Subtext>
+            <TopPaneRow>
+              <TextField
+                id="filled-basic"
+                variant="filled"
+                placeholder="Current Interactions"
+                size="small"
+              />
+            </TopPaneRow>
+            <Subtext>social interactions per day, week, or month</Subtext>
+            <Button>Start</Button>
+          </TopPane>
+          <TopPane>
+            <TopPaneHeader>Projected Outcomes</TopPaneHeader>
+            <TopPaneRow>Date hospitals overloaded</TopPaneRow>
+            <TopPaneRow>Total Infections</TopPaneRow>
+            <TopPaneRow>Total Deaths</TopPaneRow>
+          </TopPane>
+        </TopRow>
+        <BottomRow>
+          <MapGraphContainer>
+            <Map></Map>
+            <Graph></Graph>
+          </MapGraphContainer>
+        </BottomRow>
+      </Container>
+    </ThemeProvider>
+  )
 }
---
+++ client/src/styles.js
@@ -0,0 +1,68 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 1000px;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+`
+
+export const Header = styled.h1`
+  margin: 0;
+  padding: 0;
+  font-size: 44px;
+  font-weight: normal;
+`
+
+export const SubHeader = styled.h1`
+  margin: 0;
+  padding: 0;
+  font-size: 16px;
+  font-weight: normal;
+`
+
+export const TopRow = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+export const TopPane = styled.div`
+  border: 1px solid black;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  padding: 16px;
+`
+
+export const TopPaneHeader = styled.h3`
+  margin: 0;
+  padding: 0;
+  font-size: 16px;
+  font-weight: bold;
+`
+
+export const TopPaneRow = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+export const Subtext = styled.div`
+  font-size: 12px;
+  font-weight: normal;
+  color: rgba(0, 0, 0, 0.54);
+`
+
+export const BottomRow = styled.div`
+  border: 1px solid black;
+  display: flex;
+  flex-direction: column;
+`
+
+export const MapGraphContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+export const Map = styled.div``
+
+export const Graph = styled.div``
