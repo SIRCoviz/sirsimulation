b11274fd33327d986c83045adc185242bf5256c0
Peter Murphy <pete@simspace.com>
2020-03-14 18:15:09 -0400

    Modify StateMap

---
+++ client/src/components/StateMap.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useRef, useState } from 'react'
+
+const DIM = 800
+const apiUrl =
+  process.env.NODE_ENV === 'production'
+    ? process.env.REACT_APP_PROD_API_URL
+    : process.env.REACT_APP_DEV_API_URL
+
+export default ({ step = 0, initialCases = 5, scaling = 10 }) => {
+  const canvasRef = useRef()
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    fetch(apiUrl + `/initialstate/${initialCases}/${scaling}`)
+      .then(res => res.json())
+      .then(x => (console.log(x), x))
+      .then(setData)
+      .catch(console.error)
+  }, [])
+  useEffect(() => {
+    if (canvasRef.current && !!data) {
+      const scalar = DIM / data[step].cells.length
+      const ctx = canvasRef.current.getContext('2d')
+      data[step].cells.forEach((row, i) =>
+        row.forEach((d, j) => {
+          ctx.fillStyle =
+            {
+              Infected: 'red',
+              Susceptible: 'yellow',
+              Recovered: 'blue',
+              Dead: 'black',
+            }[d] || 'white'
+          ctx.fillRect(i * scalar, j * scalar, scalar, scalar)
+        })
+      )
+    }
+  }, [data])
+  return <canvas ref={canvasRef} width={DIM} height={DIM} />
+}
